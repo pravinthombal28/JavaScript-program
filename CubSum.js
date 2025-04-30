53e4152fec86d9557052428cb4048a2ee5c0bce6
pravin <thombalpravin5751@gmail.com>
2025-04-30 09:44:54 +0530

    Check Even number

---
+++ CubSum.js
@@ -0,0 +1,9 @@
+let no=3;
+let sum=0;
+let cub=0;
+for(let i=1;i<=no;i++)
+{
+	cub=i*i*i;
+	sum=sum+cub;
+}
+console.log("sum of all no cub: ",sum);
---
+++ EvenNum.js
@@ -0,0 +1,9 @@
+let num=10;
+
+for(let i=1;i<=num;i++)
+{
+	if(i%2==0)
+	{
+		console.log(i);
+	}
+}
