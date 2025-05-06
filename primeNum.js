e657f7547432d0c11b9da3c38a919ea41f1fd76d
pravin <thombalpravin5751@gmail.com>
2025-05-06 09:56:06 +0530

    prime number

---
+++ primeNum.js
@@ -0,0 +1,19 @@
+let num=15;
+
+for(let i=2;i<=num;i++)
+{	let flag=true;
+	
+	for(let j=2;j<=i/2;j++)
+	{
+		if(i%j==0)
+		{
+			flag=false;
+			break;
+		}
+	}
+
+	if(flag)
+	{
+		console.log(i);
+	}
+}
